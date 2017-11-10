94cb29f30d9a2847f22e34cbdd7c6d0616b3c9c5
Samuel <sbackstrom95@gmail.com>
2017-11-10 10:02:24 +0100

    Finished Assignment 1.

--- 17HT - 1DV506/src/sb223jy_lab1/Print.java
+++ 17HT - 1DV506/src/sb223jy_lab1/Print.java
@@ -4,9 +4,7 @@ public class Print {
     public static void main(String[] args) {
         System.out.println("Kunskap är makt!\n");
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Kunskap är makt!");
-        }
+        System.out.println("Kunskap\när\nmakt!");
 
         System.out.println("\n|==================|");
 
